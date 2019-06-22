9b9ccd7926276b152b6901acf372b004ad2a01b9
mengli11235 <mengli11235@gmail.com>
2019-06-22 15:20:04 +0200

    mismatch function

--- Five-card-draw/Core ACT-R/Model.swift
+++ Five-card-draw/Core ACT-R/Model.swift
@@ -104,8 +104,8 @@ class Model {
     When you want to use partial matching, override this function when you subclass Model
     */
     func mismatchFunction(x: Value, y: Value) -> Double? {
-        if abs(x-y) < 0.1 {
-            return -abs(x-y)
+        if fbs(x-y) < 0.1 {
+            return -fbs(x-y)
         } else {
             return -1
         }
